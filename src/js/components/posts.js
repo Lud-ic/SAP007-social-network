90958f1785544836e94a772db6a15eeadd12c2ab
lud-ic <mag.ludmila@gmail.com>
2022-04-09 22:19:26 -0300

    refactor: template string of the posts to be reusable
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: TailaMartins <tailamartins74@gmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/js/components/posts.js
+++ src/js/components/posts.js
@@ -1,11 +1,13 @@
 export function gettingPosts(city, country, message) {
   // const container2 = document.createElement("div");
 
-  const template2 = `
+export function gettingPosts(city, country, message, date) {
+  const templatePosts = `
       <div class="teste">
-        <p>${city.value}</p>
-        <p>${country.value}</p>
-        <p>${message.value}</p>
+        <p>${city}</p>
+        <p>${country}</p>
+        <p>${message}</p>
+        <p>${date}</p>
       </div>`;
   return template2;
 }
