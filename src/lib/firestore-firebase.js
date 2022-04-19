0ee8962d812eb2793065eca09a8f6f780f3ff2a5
lud-ic <mag.ludmila@gmail.com>
2022-04-18 22:25:59 -0300

    feat: add delete post
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: TailaMartins <tailamartins74@gmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/lib/firestore-firebase.js
+++ src/lib/firestore-firebase.js
@@ -5,44 +5,42 @@ import {
   getDocs,
   orderBy,
   query,
- // deleteDoc,
-} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js";
+  doc,
+  deleteDoc,
 
 const db = getFirestore();
 
 export async function addPosts(city, country, message, userEmail) {
   try {
-
     const docRef = await addDoc(collection(db, "posts"), {
-      city: city,
-      country: country,
-      message: message,
-      userEmail: userEmail,
-      date: new Date().toLocaleString("pt-br")
+      city,
+      country,
+      message,
+      userEmail,
+      date: new Date().toLocaleString("pt-br"),
     });
+
     console.log("Document written with ID: ", docRef.id);
+    return docRef.id;
   } catch (e) {
     console.error("Error adding document: ", e);
+    return null;
   }
 }
 
-
 export const getPosts = async () => {
   const arrPosts = [];
-  const orderFirestore = query(collection(db, "posts"), orderBy("date"))
+  const orderFirestore = query(collection(db, "posts"), orderBy("date"));
   const querySnapshot = await getDocs(orderFirestore);
-  querySnapshot.forEach((doc) => {
-    const timeline = doc.data();
-
+  querySnapshot.forEach((item) => {
+    const timeline = item.data();
+    timeline.id = item.id;
     arrPosts.push(timeline);
-
   });
 
-
   return arrPosts;
-
 };
 
-
-
-// export const deletePosts = await deleteDoc(doc(db, "posts", " userEmail"));
+export function deletePosts(item) {
+  return deleteDoc(doc(db, "posts", item));
+}
