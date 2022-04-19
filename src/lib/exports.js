55fc2c6962f10fb6fc504a9734db253873c6aa09
lud-ic <mag.ludmila@gmail.com>
2022-04-18 22:39:26 -0300

    style: fix eslint errors
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: TailaMartins <tailamartins74@gmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

---
+++ src/lib/exports.js
@@ -0,0 +1,28 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js"; // eslint-disable-line
+
+export {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+};
+
+export {
+  collection,
+  addDoc,
+  getFirestore,
+  getDocs,
+  orderBy,
+  query,
+  doc,
+  deleteDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js"; // eslint-disable-line
