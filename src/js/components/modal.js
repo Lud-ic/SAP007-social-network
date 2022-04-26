9fd7e510aa96588e77ba1071f74c18aed0ae6aed
lud-ic <mag.ludmila@gmail.com>
2022-04-25 21:58:29 -0300

    feat: delete post confirmation modal
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: TailaMartins <tailamartins74@gmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -7,11 +7,11 @@ export function modalEditPost(post) {
   <div id=modal class="modal">
     <div id=modalContent class="modal-content">
       <div class="message-typing-container">
-      <input value="${post.city}" type="text" id="city" class="message-typing" autocomplete="on" placeholder="Cidade"/>
-      <input value="${post.country}" type="text" id="country" class="message-typing" autocomplete="on" placeholder="País"/>
-      <textarea name="textarea" rows="5" cols="30" id="message" class="message message-typing" placeholder="Compartilhe sua experiência aqui">${post.message}</textarea>
+        <input value="${post.city}" type="text" id="city" class="message-typing" autocomplete="on" placeholder="Cidade"/>
+        <input value="${post.country}" type="text" id="country" class="message-typing" autocomplete="on" placeholder="País"/>
+        <textarea name="textarea" rows="5" cols="30" id="message" class="message message-typing" placeholder="Compartilhe sua experiência aqui">${post.message}</textarea>
       </div>
-        <div class="button-submit-container">
+      <div class="button-submit-container">
         <button id="buttonSubmit" class="button-submit-feed">Salvar</button>
       </div>
     </div>
@@ -41,11 +41,13 @@ export function modalDeletePost(post, postContainer) {
 
   const template = `
   <div id=modal class="modal">
-    <div>
-      <p>Você tem certeza que deseja excluir a postagem?</p>
-      <div>
-        <button id="button-yes" class="button-confirm-delete">Sim</button>
-        <button id="button-no" class="button-confirm-delete">Não</button>
+    <div class="modal-content">
+      <div class="modal-delete-container" >
+        <p>Você tem certeza que deseja excluir a postagem?</p>
+        <div>
+          <button id="button-yes" class="button-confirm-delete">Sim</button>
+          <button id="button-no" class="button-confirm-delete">Não</button>
+        </div>
       </div>
     </div>
   </div>
@@ -63,12 +65,12 @@ export function modalDeletePost(post, postContainer) {
   });
 
   buttonNo.addEventListener("click", () => {
-    modal.classList.add("close-modal");
+    modalContainer.remove();
   });
 
   window.addEventListener("click", (e) => {
     if (e.target === modal) {
-      modal.classList.add("close-modal");
+      modalContainer.remove();
     }
   });
 
