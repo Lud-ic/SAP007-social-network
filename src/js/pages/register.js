b0ac0b51844a629a91a3761af9ae1d9a1198bb4f
Taila Martins <tailamartins74@gmail.com>
2022-04-26 20:30:55 -0300

    feat: error messages signin e register
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: TailaMartins <tailamartins74@gmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/js/pages/register.js
+++ src/js/pages/register.js
@@ -15,7 +15,10 @@ export default function register() {
         <label class=label-password>Senha</label>
         <input type="password" class="password" id="password" minlength="6"required/>
         <label class="label-password confirmation">Confirme sua senha</label>
-        <input type="password" class="password" id="password" minlength="6"required/>
+        <input type="password" class="password" id="confirm-password" minlength="6"required/>
+        <div class="error-container">
+          <p id="error" class="error"></p>
+        </div>
         <button class="buttonSubmit btn-register" type="submit">Cadastrar</button>
         <p class="text-p"> <a href="#signin">Ja tem uma conta? </a></p>
       </form>
@@ -30,18 +33,34 @@ export default function register() {
 
   const email = container.querySelector("#email");
   const password = container.querySelector("#password");
+  const confirmPassword = container.querySelector("#confirm-password");
+  const errorFound = container.querySelector("#error");
 
   container.addEventListener("submit", (e) => {
     e.preventDefault();
-    userCreate(email.value, password.value)
-      .then(() => {
+    if (password.value === confirmPassword.value) {
+      userCreate(email.value, password.value).then(() => {
         window.location.hash = "#timeLine";
       })
-      .catch((error) => {
-        const errorMessage = error.message;
-        alert("Deu errado!");
-        return errorMessage;
-      });
+        .catch((error) => {
+          const errorCode = error.code;
+          errorFound.innerHTML = "";
+          switch (errorCode) {
+            case "auth/email-already-in-use":
+              errorFound.innerHTML = "usuário já cadastrado";
+              break;
+            case "auth/invalid-email":
+              errorFound.innerHTML = "email inválido";
+              break;
+            default:
+              errorFound.innerHTML = "ocorreu um erro, tente novamente";
+          }
+          return errorCode;
+        });
+    } else {
+      errorFound.innerHTML = "";
+      errorFound.innerHTML = "senhas incompatíveis";
+    }
   });
   return container;
 }
