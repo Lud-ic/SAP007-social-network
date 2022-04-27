5200a3e0a3c714dca85547a701e7231dbf5bc3d1
Elizenai Silva <elizenai.silva@hotmail.com>
2022-04-26 22:03:37 -0300

    test: userCreate function
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: TailaMartins <tailamartins74@gmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- test/lib/auth-firebase.spec.js
+++ test/lib/auth-firebase.spec.js
@@ -1,28 +1,33 @@
 import { userCreate } from "../../src/lib/auth-firebase.js";
-import { createUserWithEmailAndPassword } from "../../src/lib/exports.js";
+import * as exportsTest from "../../src/lib/exports.js";
 
 jest.mock("../../src/lib/exports.js");
 
 it("userCreate should create an user with email and password", async () => {
-  createUserWithEmailAndPassword.mockResolvedValue({
-    user: {},
+  exportsTest.createUserWithEmailAndPassword.mockResolvedValue({
+    user: {
+      // userEmail: "teste@teste.com",
+      // password: "123456",
+    },
   });
 
   const user = await userCreate("teste@teste.com", "123456");
+  console.log(user);
 
-  expect(createUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
-  // verificando se foi chamado com email, verificar se o usuario 'e o que mockou acima
+  expect(exportsTest.createUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
+  // verificando se foi chamado com email, verificar se o usuario é o que mockou acima
+  expect(exportsTest.createUserWithEmailAndPassword).toHaveBeenCalledWith("teste@teste.com", "123456");
 });
 
 it("userCreate should fail creating user and return error code", async () => {
   jest.clearAllMocks();
-  createUserWithEmailAndPassword.mockRejectedValue({
+  exportsTest.createUserWithEmailAndPassword.mockRejectedValue({
     code: "qualquer coisa",
   });
   try {
     await userCreate("teste@teste.com", "123456");
   } catch (error) {
-    expect(createUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
+    expect(exportsTest.createUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
     expect(error.code).toBe("qualquer coisa");
     // verificar que foi chamado com  usuario e senha
   }
